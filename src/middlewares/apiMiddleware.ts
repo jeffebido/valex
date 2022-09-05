a19f69d8f198eb6d74106cfe5e365bfd293660a4
Jefferson Oliveira <jeffebido@hotmail.com>
2022-09-05 15:21:45 -0300

    Checa api

---
+++ src/middlewares/apiMiddleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import * as repositorie from '../repositories/companyRepository';
+
+export async function apiMiddleware(req: Request, res: Response, next: NextFunction) {
+
+
+    const apiKey : any = req.headers['x-api-key'];
+
+    const company = await  repositorie.findByApiKey(apiKey);
+    
+    if ( !company ) {
+        return res.sendStatus(401);
+    }
+    next();
+} 
--- src/routes/cardsRouter.ts
+++ src/routes/cardsRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { cardsMiddleware } from "../middlewares/cardsMiddleware";
+import { apiMiddleware } from "../middlewares/apiMiddleware";
 import { newCard } from "../controllers/cardsController";
 
 const router = Router();
 
 
 
-router.post("/cards/new", cardsMiddleware, newCard);
+router.post("/cards/new", apiMiddleware, cardsMiddleware, newCard);
 
 export default router;
